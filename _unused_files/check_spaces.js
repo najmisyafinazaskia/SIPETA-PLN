99ec4631261aa24f37bbb8d10d3a8e5313f4aa6c
Najmi Syafina Zaskia <najmizaski@mhs.usk.ac.id>
2026-02-20 12:10:18 +0700

    chore: remove redundant backups and unused files

--- _unused_files/check_spaces.js
+++
@@ -1,8 +0,0 @@
-
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('d:\\PKL\\sipeda-pln\\sipeda-kp\\backend\\data\\geojson\\aceh_kabupaten.geojson', 'utf8'));
-const names = data.features.map(f => {
-    const name = f.properties.Kab_Kota || f.properties.KAB_KOTA;
-    return `|${name}|`;
-});
-console.log(JSON.stringify(names, null, 2));
